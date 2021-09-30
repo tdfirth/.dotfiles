d32f5182dcc8292aa668bdefaf4339de75992b90
Tom Firth <thomas.d.firth@gmail.com>
2021-09-30 12:06:46 +0100

    chore: configure neoformat

--- .config/nvim/lua/config/neoformat.lua
+++ .config/nvim/lua/config/neoformat.lua
@@ -1,23 +1,25 @@
 -- autoformat
-if O.format_on_save then
-  require("utils").define_augroups {
-    autoformat = {
-      {
-        "BufWritePre",
-        "*",
-        [[try | undojoin | Neoformat | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat | endtry]],
-      },
-    },
-  }
+local format_cmd = function(formatter)
+  local cmd = [[try | undojoin | Neoformat ]] .. formatter ..
+                [[ | catch /^Vim\%((\a\+)\)\=:E790/ | finally | silent Neoformat ]] ..
+                formatter .. [[ | endtry]]
+  return cmd
 end
+
+require("utils").define_augroups {
+  autoformat = {
+    { "BufWritePre", "*.lua", format_cmd("customluaformat") },
+    { "BufWritePre", "*.py", format_cmd("black") }, {
+      "BufWritePre", "*.js,*.ts,*.jsx,*.tsx,*.html,*.json,*.yaml",
+      format_cmd("prettier")
+    }
+  }
+}
 
 vim.g.neoformat_run_all_formatters = 0
 
+vim.g.neoformat_enabled_lua = { "customluaformat" }
 vim.g.neoformat_enabled_python = { "autopep8", "yapf", "docformatter" }
 vim.g.neoformat_enabled_javascript = { "prettier" }
-
-if not O.format_on_save then
-  vim.cmd [[if exists('#autoformat#BufWritePre')
-	:autocmd! autoformat
-	endif]]
-end
+vim.g.neoformat_enabled_typescript = { "prettier" }
+vim.g.neoformat_enabled_html = { "prettier" }
