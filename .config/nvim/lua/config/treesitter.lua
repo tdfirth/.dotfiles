c48b08d0986a66aa71a7df7096fc502063dd1d7e
Tom Firth <thomas.d.firth@gmail.com>
2022-01-04 17:21:53 +0000

    chore: update treesitter config and remove vim rooter

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -1,14 +1,17 @@
 local M = {}
 
-M.config = function ()
+M.config = function()
   require("nvim-treesitter.configs").setup {
-    ensure_installed = { "go", "rust", "toml", "query", "html", "typescript", "tsx" },
+    ensure_installed = {
+      "go", "rust", "toml", "query", "html", "typescript", "tsx", "yaml", "vim",
+      "lua", "json"
+    },
 
     highlight = {
       enable = true,
       use_languagetree = false,
-      disable = { "json" },
-      custom_captures = custom_captures,
+      disable = {},
+      custom_captures = custom_captures
     },
 
     incremental_selection = {
@@ -17,13 +20,13 @@ M.config = function ()
         init_selection = "<leader>s", -- maps in normal mode to init the node/scope selection
         node_incremental = "<leader>s", -- increment to the upper named parent
         node_decremental = "<M-C-s>", -- decrement to the previous node
-        scope_incremental = "<M-e>", -- increment to the upper scope (as defined in locals.scm)
-      },
+        scope_incremental = "<M-e>" -- increment to the upper scope (as defined in locals.scm)
+      }
     },
 
     indent = {
       enable = false
-    },
+    }
   }
 end
 
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,29 +4,31 @@ local fn = vim.fn
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+  execute("!git clone https://github.com/wbthomason/packer.nvim " ..
+            install_path)
   execute "packadd packer.nvim"
 end
 
 local packer_ok, packer = pcall(require, "packer")
-if not packer_ok then
-  return
-end
+if not packer_ok then return end
 
 packer.init {
-  git = { clone_timeout = 300 },
+  git = {
+    clone_timeout = 300
+  },
   display = {
     open_fn = function()
-      return require("packer.util").float { border = "single" }
-    end,
-  },
+      return require("packer.util").float {
+        border = "single"
+      }
+    end
+  }
 }
 
 return require("packer").startup(function(use)
   -- Packer can manage itself as an optional plugin
   use "wbthomason/packer.nvim"
 
-  use "airblade/vim-rooter"
   use "tpope/vim-dispatch"
   use "tpope/vim-eunuch"
   use "tpope/vim-commentary"
@@ -41,43 +43,38 @@ return require("packer").startup(function(use)
   use { "tjdevries/astronauta.nvim" }
   use {
     "tdfirth/telescope.nvim",
-    config = [[require("config.telescope")]],
+    config = [[require("config.telescope")]]
   }
-  use {"nvim-telescope/telescope-fzf-native.nvim", run = "make" }
+  use {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    run = "make"
+  }
 
   -- Treesitter
   use {
     "nvim-treesitter/nvim-treesitter",
-    config = function()
-      require("config.treesitter").config()
-		end,
+    config = function() require("config.treesitter").config() end
   }
 
   -- Neoformat
   use {
     "sbdchd/neoformat",
-    config = function()
-      require "config.neoformat"
-    end,
-    event = "BufRead",
+    config = function() require "config.neoformat" end,
+    event = "BufRead"
   }
 
   -- LANGUAGE SPECIFIC GOES HERE
   use {
     "lervag/vimtex",
     ft = "tex",
-    config = function()
-      require "config.vimtex"
-    end,
+    config = function() require "config.vimtex" end
   }
 
   use "fatih/vim-go"
   use {
     "simrat39/rust-tools.nvim",
-    disable = not O.lang.rust.rust_tools.active,
+    disable = not O.lang.rust.rust_tools.active
   }
 
-  for _, plugin in pairs(O.user_plugins) do
-    packer.use(plugin)
-  end
+  for _, plugin in pairs(O.user_plugins) do packer.use(plugin) end
 end)
