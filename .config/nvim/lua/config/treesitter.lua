174100b9d28acb0211faf5ebcab8cb36083a0521
Tom Firth <thomas.d.firth@gmail.com>
2022-01-04 16:26:18 +0000

    feat: theme and remove which key

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.config = function ()
   require("nvim-treesitter.configs").setup {
-    ensure_installed = { "go", "rust", "toml", "query", "html", "typescript", "tsx", "elixir" },
+    ensure_installed = { "go", "rust", "toml", "query", "html", "typescript", "tsx" },
 
     highlight = {
       enable = true,
@@ -14,9 +14,9 @@ M.config = function ()
     incremental_selection = {
       enable = true,
       keymaps = {
-        init_selection = "<leader>w", -- maps in normal mode to init the node/scope selection
-        node_incremental = "<leader>w", -- increment to the upper named parent
-        node_decremental = "<M-C-w>", -- decrement to the previous node
+        init_selection = "<leader>s", -- maps in normal mode to init the node/scope selection
+        node_incremental = "<leader>s", -- increment to the upper named parent
+        node_decremental = "<M-C-s>", -- decrement to the previous node
         scope_incremental = "<M-e>", -- increment to the upper scope (as defined in locals.scm)
       },
     },
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,25 +4,22 @@ local fn = vim.fn
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute("!git clone https://github.com/wbthomason/packer.nvim " ..
-            install_path)
+  execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
   execute "packadd packer.nvim"
 end
 
 local packer_ok, packer = pcall(require, "packer")
-if not packer_ok then return end
+if not packer_ok then
+  return
+end
 
 packer.init {
-  git = {
-    clone_timeout = 300
-  },
+  git = { clone_timeout = 300 },
   display = {
     open_fn = function()
-      return require("packer.util").float {
-        border = "single"
-      }
-    end
-  }
+      return require("packer.util").float { border = "single" }
+    end,
+  },
 }
 
 return require("packer").startup(function(use)
@@ -36,72 +33,51 @@ return require("packer").startup(function(use)
 
   -- Theme
   use "kyazdani42/nvim-web-devicons"
-  use "sainnhe/everforest"
-  use "andreypopp/vim-colors-plain"
+  use "tdfirth/herdwick"
 
-  -- use "neovim/nvim-lspconfig"
-  -- use "kabouzeid/nvim-lspinstall"
   -- Telescope
   use { "nvim-lua/popup.nvim" }
   use { "nvim-lua/plenary.nvim" }
   use { "tjdevries/astronauta.nvim" }
   use {
     "tdfirth/telescope.nvim",
-    config = [[require("config.telescope")]]
+    config = [[require("config.telescope")]],
   }
-  use {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    run = "make"
-  }
-  -- Autocomplete
-  -- use {
-  --   "hrsh7th/nvim-cmp",
-  --   event = "InsertEnter",
-  --   config = function()
-  --     require("config.cmp").config()
-  --   end,
-  -- }
-  -- use { "hrsh7th/cmp-buffer" }
-  -- use { "hrsh7th/cmp-path" }
+  use {"nvim-telescope/telescope-fzf-native.nvim", run = "make" }
 
   -- Treesitter
   use {
     "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-    config = function() require("config.treesitter").config() end
+    config = function()
+      require("config.treesitter").config()
+		end,
   }
 
   -- Neoformat
   use {
     "sbdchd/neoformat",
-    config = function() require "config.neoformat" end,
-    event = "BufRead"
-  }
-
-  -- whichkey
-  use {
-    "folke/which-key.nvim",
-    config = function() require "config.which-key" end,
-    event = "BufWinEnter"
+    config = function()
+      require "config.neoformat"
+    end,
+    event = "BufRead",
   }
 
   -- LANGUAGE SPECIFIC GOES HERE
   use {
     "lervag/vimtex",
     ft = "tex",
-    config = function() require "config.vimtex" end
+    config = function()
+      require "config.vimtex"
+    end,
   }
 
-  use {
-    "fatih/vim-go",
-    config = function() require "config.vim-go" end
-  }
+  use "fatih/vim-go"
   use {
     "simrat39/rust-tools.nvim",
-    disable = not O.lang.rust.rust_tools.active
+    disable = not O.lang.rust.rust_tools.active,
   }
 
-  use { "elixir-editors/vim-elixir" }
-
-  for _, plugin in pairs(O.user_plugins) do packer.use(plugin) end
+  for _, plugin in pairs(O.user_plugins) do
+    packer.use(plugin)
+  end
 end)
