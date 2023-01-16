b479e590b71e805474dadf98fba479be82bd400a
Tom Firth <thomas.d.firth@gmail.com>
2023-01-16 09:45:50 +0000

    Finish migrating keymaps

--- .config/nvim/lua/tdfirth/keymaps.lua
+++ .config/nvim/lua/tdfirth/keymaps.lua
@@ -1,9 +1,3 @@
-local keymap = vim.api.nvim_set_keymap
-
-local function leader(seq)
-	return "<leader>" .. seq
-end
-
 local keymaps = {
 	i = {
 		ctrl = {},
@@ -11,7 +5,7 @@ local keymaps = {
 		alt = {},
 		leader = {},
 		g = {},
-		lit = {},
+		_ = {},
 	},
 	n = {
 		ctrl = {},
@@ -19,25 +13,25 @@ local keymaps = {
 		alt = {},
 		leader = {},
 		g = {},
-		lit = {},
+		_ = {},
 	},
 	v = {
 		ctrl = {},
 		leader = {},
 		g = {},
-		lit = {},
+		_ = {},
 	},
 	t = {
 		ctrl = {},
 		leader = {},
 		g = {},
-		lit = {},
+		_ = {},
 	},
 }
 
 -- Set the leader
 vim.g.mapleader = " "
-keymaps.n.lit["<Space>"] = { "<NOP>" }
+keymaps.n._["<Space>"] = { "<NOP>" }
 
 -- Ctrl-C to escape
 keymaps.n.ctrl["c"] = { "<ESC>", { silent = true } }
@@ -50,10 +44,10 @@ keymaps.n.ctrl["k"] = { "<C-w>k", { silent = true } }
 keymaps.n.ctrl["l"] = { "<C-w>l", { silent = true } }
 
 -- Move nicely through wrapped lines
-keymaps.n.lit["j"] = { "gj" }
-keymaps.n.lit["k"] = { "gk" }
-keymaps.v.lit["j"] = { "gj" }
-keymaps.v.lit["k"] = { "gk" }
+keymaps.n._["j"] = { "gj" }
+keymaps.n._["k"] = { "gk" }
+keymaps.v._["j"] = { "gj" }
+keymaps.v._["k"] = { "gk" }
 
 -- Terminal window navigation
 keymaps.t.ctrl["h"] = { "<C-\\><C-N><C-w>h" }
@@ -64,15 +58,15 @@ keymaps.i.ctrl["h"] = { "<C-\\><C-N><C-w>h" }
 keymaps.i.ctrl["j"] = { "<C-\\><C-N><C-w>j" }
 keymaps.i.ctrl["k"] = { "<C-\\><C-N><C-w>k" }
 keymaps.i.ctrl["l"] = { "<C-\\><C-N><C-w>l" }
-keymaps.t.lit["<Esc>"] = { "<C-\\><C-n>" }
+keymaps.t._["<Esc>"] = { "<C-\\><C-n>" }
 
 -- Better indenting
-keymaps.v.lit["<"] = { "<gv" }
-keymaps.v.lit[">"] = { ">gv" }
+keymaps.v._["<"] = { "<gv" }
+keymaps.v._[">"] = { ">gv" }
 
 -- Tab switch buffer
 keymaps.n.shift["TAB"] = { ":bprevious<CR>" }
-keymaps.n.lit["<TAB>"] = { ":bnext<CR>" }
+keymaps.n._["<TAB>"] = { ":bnext<CR>" }
 
 -- Move current line / block with Alt-j/k ala vscode.
 keymaps.n.alt["j"] = { ":m .+1<CR>==" }
@@ -81,25 +75,25 @@ keymaps.i.alt["j"] = { "<Esc>:m .+1<CR>==gi" }
 keymaps.i.alt["k"] = { "<Esc>:m .-2<CR>==gi" }
 
 -- QuickFix
-keymap("n", "]q", ":cnext<CR>", { noremap = true, silent = true })
-keymap("n", "[q", ":cprev<CR>", { noremap = true, silent = true })
+keymaps.n._["]q"] = { ":cnext<CR>" }
+keymaps.n._["[q"] = { ":cprev<CR>" }
 
 vim.cmd('vnoremap p "0p')
 vim.cmd('vnoremap P "0P')
 
 -- Close a buffer without closing the window
-keymap("n", leader("d"), ":bp<bar>sp<bar>bn<bar>bd<CR>", { noremap = true, silent = true })
+keymaps.n.leader["d"] = { ":bp<bar>sp<bar>bn<bar>bd<CR>" }
 
 -- Search
-keymap("n", leader("f"), ":Telescope find_files<CR>", { noremap = true, silent = true })
-keymap("n", "<C-b>", ":Telescope buffers<CR>", { noremap = true, silent = true })
-keymap("n", "<C-/>", ":Telescope live_grep<CR>", { noremap = true, silent = true })
-keymap(
-	"n",
-	"<C-Space>",
-	':lua require("telescope.builtin").grep_string { search = vim.fn.expand("<cword>") }<CR>',
-	{ noremap = true, silent = true }
-)
+keymaps.n.leader["f"] = { ":Telescope find_files<CR>" }
+keymaps.n.ctrl["b"] = { ":Telescope buffers<CR>" }
+keymaps.n.ctrl["/"] = { ":Telescope live_grep<CR>" }
+keymaps.n.ctrl["Space"] = {
+	function()
+		print("Grepping")
+		require("telescope.builtin").grep_string({ search = vim.get_visual_selection() })
+	end,
+}
 
 function vim.get_visual_selection()
 	local _, line_start, col_start, _ = unpack(vim.fn.getpos("'<"))
@@ -113,14 +107,6 @@ function vim.get_visual_selection()
 	lines[last] = string.sub(lines[last], 0, col_end)
 	return vim.fn.join(lines, "\n")
 end
-
-keymaps.v.ctrl["Space"] = {
-	function()
-		print("Grepping")
-		require("telescope.builtin").grep_string({ search = vim.get_visual_selection() })
-	end,
-	{ noremap = true, silent = true },
-}
 
 vim.api.nvim_create_user_command("ReloadConfig", function()
 	print("Reloading vim config...")
