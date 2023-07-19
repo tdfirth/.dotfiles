916d9d658cebfaa1ac36968c41087409b427872f
Tom Firth <thomas.d.firth@gmail.com>
2023-07-19 16:00:00 +0100

    nvim

---
+++ .config/nvim/colors/tdfirth.lua
@@ -0,0 +1,9 @@
+vim.g.colors_name = "tdfirth"
+
+package.loaded["tdfirth.theme"] = nil
+package.loaded["tdfirth.theme.base"] = nil
+package.loaded["tdfirth.theme.treesitter"] = nil
+package.loaded["tdfirth.theme.lsp"] = nil
+package.loaded["tdfirth.theme.others"] = nil
+
+require("tdfirth.theme")
