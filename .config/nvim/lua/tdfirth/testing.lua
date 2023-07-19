135ebfff6802fc91d15d04a78ba49b18e950c867
Tom Firth <thomas.d.firth@gmail.com>
2023-07-19 16:00:43 +0100

    nvim

---
+++ .config/nvim/lua/tdfirth/testing.lua
@@ -0,0 +1,17 @@
+vim.cmd([[
+let test#strategy = { 'nearest': 'dispatch', 'file': 'dispatch', 'suite': 'dispatch'}
+
+function! SetNxProject()
+  let filename = expand('%')
+  let nx_project = matchstr(filename, '\(apps\|libs\)\/\zs.*\ze\/src\/.*.ts')
+  if nx_project != ""
+    let g:test#javascript#nx#project = nx_project 
+  endif
+endfunction
+
+augroup vim_test
+    autocmd!
+    autocmd BufEnter *.ts :call SetNxProject()
+    autocmd BufEnter *.tsx :call SetNxProject()
+augroup END
+]])
