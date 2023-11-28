33941d2a95e423c177d255d9c53122cc4ea0481f
Tom Firth <thomas.d.firth@gmail.com>
2023-11-28 21:53:32 +0000

    Improve format on save

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -5,26 +5,32 @@ lsp_zero.preset("recommended")
 require("mason").setup({})
 require("mason-lspconfig").setup({
   ensure_installed = { "tsserver", "pyright" },
-  handlers = {
-    lsp_zero.default_setup,
-  },
+  handlers = { lsp_zero.default_setup }
 })
 
 local cmp = require("cmp")
 local luasnip = require("luasnip")
-local select_opts = { behavior = cmp.SelectBehavior.Select }
+local select_opts = {
+  behavior = cmp.SelectBehavior.Select
+}
 
 cmp.setup({
   snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
+    expand = function(args) luasnip.lsp_expand(args.body) end
   },
   sources = {
-    { name = "path" },
-    { name = "nvim_lsp", keyword_length = 1 },
-    { name = "buffer",   keyword_length = 3 },
-    { name = "luasnip",  keyword_length = 2 },
+    {
+      name = "path"
+    }, {
+    name = "nvim_lsp",
+    keyword_length = 1
+  }, {
+    name = "buffer",
+    keyword_length = 3
+  }, {
+    name = "luasnip",
+    keyword_length = 2
+  }
   },
   formatting = {
     fields = { "menu", "abbr", "kind" },
@@ -33,12 +39,12 @@ cmp.setup({
         nvim_lsp = "λ",
         luasnip = "⋗",
         buffer = "Ω",
-        path = "🖫",
+        path = "🖫"
       }
 
       item.menu = menu_icon[entry.source.name]
       return item
-    end,
+    end
   },
   mapping = {
     ["<C-p>"] = cmp.mapping.select_prev_item(select_opts),
@@ -48,8 +54,12 @@ cmp.setup({
     ["<C-d>"] = cmp.mapping.scroll_docs(4),
 
     ["<C-e>"] = cmp.mapping.abort(),
-    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    ["<C-y>"] = cmp.mapping.confirm({
+      select = true
+    }),
+    ["<CR>"] = cmp.mapping.confirm({
+      select = false
+    }),
 
     ["Tab"] = cmp.mapping(function(fallback)
       if luasnip.jumpable(1) then
@@ -65,53 +75,50 @@ cmp.setup({
       else
         fallback()
       end
-    end, { "i", "s" }),
+    end, { "i", "s" })
   },
   window = {
     documentation = {
       border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
       winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
       col_offset = -3,
-      side_padding = 0,
+      side_padding = 0
     },
     completion = {
       border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
       winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,Search:None",
       col_offset = -3,
-      side_padding = 0,
-    },
-  },
+      side_padding = 0
+    }
+  }
 })
 
 lsp_zero.set_sign_icons({
   error = "E",
   warn = "W",
   hint = "H",
-  info = "I",
+  info = "I"
 })
 
 vim.diagnostic.config({
-  virtual_text = false,
+  virtual_text = false
 })
 
 lsp_zero.on_attach(function(client, bufnr)
-  local opts = { buffer = bufnr, remap = true, silent = true }
+  local opts = {
+    buffer = bufnr,
+    remap = true,
+    silent = true
+  }
 
-  lsp_zero.default_keymaps({ buffer = bufnr })
-  lsp_zero.buffer_autoformat()
+  lsp_zero.default_keymaps({
+    buffer = bufnr
+  })
 
-  vim.keymap.set("n", "K", function()
-    vim.lsp.buf.hover()
-  end, opts)
-  vim.keymap.set("n", "]d", function()
-    vim.diagnostic.goto_next()
-  end, opts)
-  vim.keymap.set("n", "[d", function()
-    vim.diagnostic.goto_prev()
-  end, opts)
-  vim.keymap.set("n", "<leader>lr", function()
-    vim.lsp.buf.rename()
-  end, opts)
+  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, opts)
 end)
 
 lsp_zero.setup()
@@ -123,5 +130,8 @@ require("lspconfig").tsserver.setup({
     -- disable formatting capabilities
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentFormattingRangeProvider = false
-  end,
+  end
 })
+
+-- Format on save
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
