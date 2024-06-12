e4ef7121273b2a9fb27a72f84bb261091aaaf5f1
Tom Firth <thomas.d.firth@gmail.com>
2024-06-12 23:41:36 +0100

    Better symbol search

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -13,12 +13,19 @@ return {
 					vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
 				end
 
-				map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-				map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-				map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-				map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-				map("<leader>sd", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-				map("<leader>sf", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+				local builtin = require("telescope.builtin")
+
+				map("gd", builtin.lsp_definitions, "[G]oto [D]efinition")
+				map("gr", builtin.lsp_references, "[G]oto [R]eferences")
+				map("gI", builtin.lsp_implementations, "[G]oto [I]mplementation")
+				map("<leader>D", builtin.lsp_type_definitions, "Type [D]efinition")
+				map("<leader>sd", builtin.lsp_document_symbols, "[D]ocument [S]ymbols")
+				map("<leader>sf", function()
+					builtin.lsp_dynamic_workspace_symbols({
+						path_display = "hidden",
+						symbol_width = 64,
+					})
+				end, "[W]orkspace [S]ymbols")
 				map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 				map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 				map("K", vim.lsp.buf.hover, "Hover Documentation")
