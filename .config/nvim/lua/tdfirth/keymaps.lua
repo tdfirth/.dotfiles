61cd858dd14ff633834d40eed729ddd98575b3e5
Tom Firth <16757412+tdfirth@users.noreply.github.com>
2026-02-17 23:28:32 -0500

    Diagnostic on hover

--- .config/nvim/lua/tdfirth/keymaps.lua
+++ .config/nvim/lua/tdfirth/keymaps.lua
@@ -9,17 +9,6 @@ map("n", "k", "gk", { silent = true })
 map("v", "j", "gj", { silent = true })
 map("v", "k", "gk", { silent = true })
 
--- Window movement
-map("n", "<C-h>", "<C-w>h", { silent = true })
-map("n", "<C-j>", "<C-w>j", { silent = true })
-map("n", "<C-k>", "<C-w>k", { silent = true })
-map("n", "<C-l>", "<C-w>l", { silent = true })
-
--- Terminal window navigation
-map("t", "<C-h>", "<C-\\><C-N><C-w>h", { silent = true })
-map("t", "<C-j>", "<C-\\><C-N><C-w>j", { silent = true })
-map("t", "<C-k>", "<C-\\><C-N><C-w>k", { silent = true })
-map("t", "<C-l>", "<C-\\><C-N><C-w>l", { silent = true })
 map("t", "<Esc>", "<C-\\><C-n>", { silent = true })
 
 -- Better indenting
@@ -33,7 +22,7 @@ map("i", "<M-Down>", "<Esc>:m .+1<CR>==gi", { silent = true })
 map("i", "<M-Up>", "<Esc>:m .-2<CR>==gi", { silent = true })
 
 -- Diagnostic float
-map("n", "<M-k>", vim.diagnostic.open_float, { silent = true })
+map("n", "<M-S-k>", vim.diagnostic.open_float, { silent = true })
 
 -- Search
 map("n", "*", "*N")
